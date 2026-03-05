ca862f045ccf5fadc99f9bfbd592a84f633360e8
Boyd Myers <buddy@lastwarsurvivalbuddy.com>
2026-03-04 19:24:14 -0600

    Fix env var types

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -6,8 +6,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabase =
   typeof window !== 'undefined'
     ? createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
       )
     : null;
 
